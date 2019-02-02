65e2404afa4f244aaaba3af6f6a36953daa8ee23
acatao2210 <acatao2210@gmail.com>
2019-02-02 16:15:23 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import json
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+  log('Recieved {}'.format(data))
+
+  # We don't want to reply to ourselves!
+  if data['name'] != 'Private Baca':
+    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    send_message(msg)
+
+  return "ok", 200
+
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('ad73e79306e3ad1b3452a4a990'),
+          'text'   : 'hi'msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
+
+def log(msg):
+  print(str(msg))
+  sys.stdout.flush()
